705329d5f80b1f222eecf3cd0143ea1bd1cf6f87
Rohit Singh Rawat <117940279+Rohit-Singh-Rawat@users.noreply.github.com>
2024-03-30 15:40:36 +0000

    Starts First UI

---
+++ Ableton-ui/src/Page/Page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../Componenents/Navbar'
+
+const Page = () => {
+  return (
+    <div>
+        <Navbar>
+            
+        </Navbar>
+    </div>
+  )
+}
+
+export default Page
