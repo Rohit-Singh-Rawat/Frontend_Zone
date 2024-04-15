835b9280950484934a6197d9b2ccab6e0d252e98
Rohit Singh Rawat <rohitzrawat2003@gmail.com>
2024-04-16 00:12:02 +0530

    feat: add responsiveness to navbar

--- Ableton-ui/src/Component/Navbar.jsx
+++ Ableton-ui/src/Component/Navbar.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Navbar = () => {
 	return (
-		<header>
-			<nav className='flex justify-between border-b-2 border-[#eee] items-center pt-5 pb-[17px] px-10'>
-				<div className='flex text-[20px] gap-10 justify-center  items-center'>
+		<div>
+			<nav className='flex justify-between border-b-2 border-[#eee] items-end lg:items-center pt-[18px] pb-[19px] px-5 xl:px-10'>
+				<div className='flex  xl:text-[20px] gap-5 xl:gap-10 justify-center  items-center'>
 					<div>
 						<svg
 							role='img'
@@ -19,7 +19,11 @@ const Navbar = () => {
 							<path d='M0 0h3v21H0zM6 0h3v21H6zM12 0h3v21h-3zM18 0h3v21h-3zM24 18h21v3H24zM24 12h21v3H24zM24 6h21v3H24zM24 0h21v3H24z'></path>
 						</svg>
 					</div>
-					<div className='flex gap-7 font-medium'>
+					<div className='flex lg:hidden text-end text-xl align-text-bottom font-medium gap-2 items-end	'>
+						<span>Menu </span>
+						<div class='w-0 h-0 border-l-[4px] border-l-transparent border-t-[6.5px] border-t-black border-r-[4px] border-r-transparent'></div>
+					</div>
+					<div className='hidden lg:flex gap-5 xl:gap-7 font-medium'>
 						<a
 							href='
     '
@@ -64,25 +68,38 @@ const Navbar = () => {
 						</a>
 						<a
 							href=''
-							className='text-[#ff764d]'
+							className='text-[#ff764d] flex items-center gap-[3px]'
 						>
-							More +
+							More{' '}
+							<svg
+								xmlns='http://www.w3.org/2000/svg'
+								viewBox='0 0 12 16'
+								id='Plus'
+								className='size-4'
+							>
+								<path
+									fill-rule='evenodd'
+									d='M12 9H7v5H5V9H0V7h5V2h2v5h5z'
+									fill='#ff764d'
+									className='color000000 svgShape'
+								></path>
+							</svg>
 						</a>
 					</div>
 				</div>
 
-				<div className='flex  items-center  gap-8'>
+				<div className='hidden lg:flex items-center gap-4 xl:gap-8'>
 					<a
 						href='
                     '
-						className='text-[20px] font-medium text-[#0000ff]'
+						className='text-sm	 xl:text-[20px] font-medium text-[#0000ff]'
 					>
 						Try Live 12 for free
 					</a>
-					<a className='font-medium'>Log in or register</a>
+					<a className='font-medium text-sm xl:text-base'>Log in or register</a>
 				</div>
 			</nav>
-			<nav className='flex gap-[30px] font-medium py-[22px] px-10'>
+			<nav className='flex  xl:gap-[30px] gap-5 px-5 xl:text-base text-sm font-medium py-5 xl:py-[22px] xl:px-10'>
 				<a
 					href='
                 '
@@ -98,7 +115,7 @@ const Navbar = () => {
 					Apprenticeships
 				</a>
 			</nav>
-		</header>
+		</div>
 	);
 };
 
